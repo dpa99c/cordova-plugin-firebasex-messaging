0d56844cd636b7dab6d1450682ea15e66bb1ced6
Dave Alden <dave.alden@srt-marine.com>
2026-05-30 13:59:20 +0100

    (ios) Use SPM instead of Cocoapods for Firebase SDK on cordova-ios@8+

---
+++ Package.swift
@@ -0,0 +1,31 @@
+// swift-tools-version:5.9
+
+import PackageDescription
+
+let firebaseSDKVersion: Version = "12.9.0"
+
+let package = Package(
+    name: "cordova-plugin-firebasex-messaging",
+    platforms: [.iOS(.v13)],
+    products: [
+        .library(
+            name: "cordova-plugin-firebasex-messaging",
+            targets: ["cordova-plugin-firebasex-messaging"]
+        )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apache/cordova-ios.git", branch: "master"),
+        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", exact: firebaseSDKVersion)
+    ],
+    targets: [
+        .target(
+            name: "cordova-plugin-firebasex-messaging",
+            dependencies: [
+                .product(name: "Cordova", package: "cordova-ios"),
+                .product(name: "FirebaseMessaging", package: "firebase-ios-sdk")
+            ],
+            path: "src/ios",
+            publicHeadersPath: "."
+        )
+    ]
+)
